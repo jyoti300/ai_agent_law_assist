5ada201e1298e53f5f28a82b8724bbbcecc2f4f8
jyoti300 <kushwahajyoti157@gmail.com>
2026-02-24 20:05:57 +0530

    Initial commit - AI Law Assist

--- appointments/qr.py
+++ appointments/qr.py
@@ -1,8 +1,8 @@
 import qrcode
 
-whatsapp_link = "https://wa.me/8591403229?text=Book%20Appointment"
+whatsapp_link = "https://wa.me/8591403228?text=Book%20Appointment"
 
 img = qrcode.make(whatsapp_link)
-img.save("ai_law_assist_qr.png")
+img.save("ai_law_assist_live_qr.png")
 
 print("QR generated")
